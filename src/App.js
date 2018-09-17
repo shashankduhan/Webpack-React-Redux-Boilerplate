95a39d3da0d17545762338321d6ee05e32af471e
Shashank Duhan <Shashank.Duhan@SPI.COM>
2018-09-17 16:26:54 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './app.scss';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div className="sweet">
+                Sweet!
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              {
+                loader: "style-loader" // creates style nodes from JS strings
+              },
+              {
+                loader: "css-loader" // translates CSS into CommonJS
+              },
+              {
+                loader: "sass-loader" // compiles Sass to CSS
+              }
+            ]
+          }
+        ]
+      },
+      resolve: {
+        extensions: ['*', '.js', '.jsx']
+      },
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    devServer: {
+      contentBase: './dist'
+    }
+};
