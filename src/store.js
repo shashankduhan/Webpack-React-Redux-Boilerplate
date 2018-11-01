f401214968fb55f71fcf6526ca6b2c2e36148ef5
Shashank Duhan <milkyduhan@gmail.com>
2018-11-01 18:16:21 +0530

    Redux Dev Tool added universally

--- src/store.js
+++ src/store.js
@@ -10,13 +10,13 @@ const initialState = {
 };
 
 const middleware = [thunk, createLogger];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     rootReducer, 
     initialState, 
-    compose (
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers (
+        applyMiddleware(...middleware)
     ));
 
 export default store;
